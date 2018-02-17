6474d3d20716bac6142c5b4c2cd541861fec6550
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-17 09:42:55 +0000

    run @rob_read once to reduce test run time

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe Reader do
-  let(:rob) { Reader.new('26040396-robert-hughes') }
-  let(:rob_read) { rob.read }
+  before(:all) do
+    rob = Reader.new('26040396-robert-hughes')
+    @rob_read = rob.read
+  end
 
   describe '#read' do
     it 'is a method that returns finished books' do
-      expect(rob_read).to include('Best Served Cold')
+      expect(@rob_read).to include('Best Served Cold')
     end
 
     it 'returns more than one page of books' do
-      expect(rob_read.length).to be > 20
+      expect(@rob_read.length).to be > 20
     end
   end
 end
