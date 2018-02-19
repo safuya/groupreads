20c702203f4e7a5d5f2869b466a5b852d077f6df
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-19 12:18:12 +0000

    add cli findgroup call

--- lib/groupreads/cli.rb
+++ lib/groupreads/cli.rb
@@ -1,12 +1,20 @@
 require 'thor'
+require 'groupreads'
 
 module Groupreads
   class CLI < Thor
 
+    desc "register USER", "Adds a user from id-user-name"
     def register(name)
       reader = Groupreads::Reader.new(name)
       puts "#{reader.username} added."
     end
 
+    desc "findgroup NAME", "Search for a group"
+    def findgroup(name)
+      groups = Groupreads::Group.search(name)
+      groups.each { |group| puts group }
+    end
+    
   end
 end
