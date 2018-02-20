df354a7f57044c6961a87366e47f64ce38d176e9
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-20 10:31:57 +0000

    implement list_groups

--- lib/groupreads/reader.rb
+++ lib/groupreads/reader.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require 'pry'
 
 module Groupreads
   class Reader
@@ -15,7 +16,7 @@ module Groupreads
       res = []
       puts "Goodreads only allow collection of one page per second. Collecting page 1."
       res << Nokogiri::XML(open(
-        "https://www.goodreads.com/review/list/#{self.id}.xml?key=#{self.key}&v=2&shelf=#{name}&page=1"
+        "https://www.goodreads.com/review/list/#{self.id}.xml?key=#{self.key}&v=2&shelf=#{name}"
       ))
       iterations = res[0].xpath('//reviews/@total')[0].value.to_f / 20 - 1
       iterations.ceil.times do |i|
@@ -43,5 +44,24 @@ module Groupreads
       read + to_read
     end
 
+    def list_groups
+      res = []
+      res << Nokogiri::XML(open(
+        "https://www.goodreads.com/group/list/#{self.id}.xml?key=#{self.key}"
+      ))
+      iterations = res[0].xpath('//list/@total')[0].value.to_f / 20 - 1
+      iterations.ceil.times do |i|
+        puts "Collecting page #{i + 2} of #{iterations.ceil + 1}."
+        res << Nokogiri::XML(open(
+          "https://www.goodreads.com/group/list/#{self.id}.xml?key=#{self.key}&page=#{i + 2}"
+        ))
+      end
+      res.map do |groups|
+        groups.xpath('//group/link').map do |group|
+          group.text.gsub('https://www.goodreads.com/group/show/', '')
+        end
+      end.flatten      
+    end
+    
   end
 end
